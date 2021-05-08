75c41aa32f3cc80fd6e8640076b0ef8ffbd3776b
Darkwind <somatic20@gmail.com>
2021-05-09 00:19:04 +0100

    added comments to contract fixed bugs and tyos

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -6,6 +6,9 @@ import "./SafeMath.sol";
 import "./Ownable.sol";
 pragma experimental ABIEncoderV2;
 
+/* 
+struct contains all stakeholder details
+*/
 struct User {
     address holder;
     uint stake;
@@ -13,7 +16,9 @@ struct User {
     uint reward;
     bool isHolder;
 }
-
+/* 
+struct stores amount of users initial investment to be withdrawn 
+*/
 struct LockedStake {
     uint stake;
     uint dateLocked;
@@ -25,7 +30,11 @@ contract Staking is ERC20, Ownable {
     mapping(address => LockedStake) internal lockedStakes;
     uint _totalStakes;
     uint APY = 50;
-
+    uint WAIT_PERIOD = 2 days;
+/*
+initalizing amount of token supply in circulation
+passing constructor value for erc 20 contract
+*/
     constructor(
         address _owner,
         uint _supply,
@@ -37,12 +46,15 @@ contract Staking is ERC20, Ownable {
         _mint(_owner, _supply);
     }
 
-    event createStakeEvent(User user);
-
+    // event createStakeEvent(User user);
+/*
+function creates a new struct object to track stakes and 
+updats object if one exists already.
+tokens are burnt to reduce total supply and create scarcity
+ */
     function createStake(uint stake) public {
-        _burn(msg.sender, stake);
+        _burn(msg.sender, stake);  
         _totalStakes.add(stake);
-
        if(!isStakeHolder(msg.sender)){
            User memory user =
             User({
@@ -57,14 +69,19 @@ contract Staking is ERC20, Ownable {
        }else {
            User storage oldUser = users[msg.sender];
            oldUser.stake = oldUser.stake.add(stake);
-       }        
+       }     
     }
-
+/* 
+users can initate process to withdraw iinital investment without rewards  here
+the funds will be locked for a period of time before finally being released
+by calling RemoveStake ().
+If user tries to unstake more of their inital investements, the timer will be reset
+once a user has 0 stakes.
+*/
     function InitiateRemoveStake(uint stake) public {
-        require(isStakeHolder(msg.sender) == true, 'Not a stakeholder');
-        User storage user = users[msg.sender];        
+        require(isStakeHolder(msg.sender) == true, 'Not a stakeholder');    
         require(address(this).balance >= stake, 'Not enough tokens');
-
+   
         LockedStake storage lockedStake = lockedStakes[msg.sender];
         if(!lockedStake.locked) {
             LockedStake memory lockedStake = 
@@ -79,28 +96,36 @@ contract Staking is ERC20, Ownable {
             lockedStake.dateLocked = lockedStake.dateLocked = now;
         }        
         
+        User storage user = users[msg.sender]; 
         user.stake = user.stake.sub(stake);
-        if (user.stake == 0) 
-            removeStakeHolder(msg.sender);
-        _mint(msg.sender, stake);
     }
-
+/* 
+locked stakes are transfered to users address
+total stakes is also reduced
+*/
     function RemoveStake () public {
         LockedStake storage lockedStake = lockedStakes[msg.sender];
         require(lockedStake.locked == true, 'No stakes locked');
+        require(lockedStake.dateLocked > WAIT_PERIOD, 'waiting period has not been exceeded');
         uint amountUnstaked = lockedStake.stake;
         transfer(msg.sender, amountUnstaked);
+        _totalStakes.sub(amountUnstaked);
         lockedStake.stake = lockedStake.stake.sub(amountUnstaked);
+        lockedStake.locked = false;
+        _mint(msg.sender, amountUnstaked);
     }
 
+//returns current stake of a user
     function stakeOf(address stakeHolder) public view returns (uint) {
         return users[stakeHolder].stake;
     }
 
+//returns total number of stakes in the contract
     function totalStakes() public view returns (uint) {
         return _totalStakes;
     }
 
+//check if a user is a holder
     function isStakeHolder (address _address)
         internal
         view
@@ -109,32 +134,37 @@ contract Staking is ERC20, Ownable {
         return users[_address].isHolder;
     }
 
-    function removeStakeHolder(address stakeHolder) internal {
-        require(isStakeHolder(msg.sender) == true, 'Not a stakeholder');
-        users[stakeHolder].isHolder = false;
-    }
-
+//returns how much rewards a user has made
     function rewardOf(address stakeHolder) external view returns (uint) {        
         require(isStakeHolder(msg.sender) == true, 'Not a stakeholder');
         return users[stakeHolder].reward;
     }
 
-    function withdrawReward(uint amount) external {
-        require(address(this).balance >= amount, 'Not enough tokens');
+/*
+user can withdraw reward gained here
+once reward amount is available, it is transfered to users address
+*/
+    function withdrawReward(uint amount) external {      
+        require(isStakeHolder(msg.sender) == true, 'Not a stakeholder');        
         User storage user  = users[msg.sender];
-        user.reward = 0;
-        _mint(msg.sender, user.reward);
+        require(user.reward >= amount, 'Not enough tokens');
+        transfer(msg.sender, amount);
+        user.reward = user.reward.sub(amount);
     }
-
+/* 
+function is called from an oracle once a day with a certain apy.
+we then calculate ROI for that day based on users stake and current apy, that value 
+is then added to users total reward
+*/
     function calculateRewardBasedOnApy(uint apy) public {
         require(isStakeHolder(msg.sender) == true, 'Not a stakeholder');
-        User storage user = users[msg.sender];
         require(lockedStakes[msg.sender].locked == true, 'Stakes locked, cannot earn rewards');
         uint totalUserStake = stakeOf(msg.sender);
         require(totalUserStake < 0, 'stake is too low');
         uint apyPercentage = apy.div(100);
         uint reward = totalUserStake.mul(apyPercentage);
-        uint finalAmount = reward / 365;
-        user.reward.add(finalAmount);
+        uint roi = reward / 365;
+        User storage user = users[msg.sender];
+        user.reward.add(roi);
     }
 }
